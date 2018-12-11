3687237e0ca2798b95a4170beaf8cbb0a461b311
Carsten Singleton <36901825+carstensing@users.noreply.github.com>
2018-12-10 20:07:33 -0800

    Updated documentation

--- src/edu/miracosta/cs113/Tile.java
+++ src/edu/miracosta/cs113/Tile.java
@@ -7,38 +7,72 @@ public class Tile {
     private int width;
     private int height;
 
+    /**
+    * Constructor.
+    * @param Color color color of tile
+    */
     public Tile(Color color) {
         this.color = color;
         this.width = 5;
         this.height = 5;
     }
 
+    /**
+    * Full constructor.
+    * @param Color color color of tile
+    * @param int width tile width
+    * @param int height tile height
+    */
     public Tile(Color color, int width, int height) {
         this.color = color;
         this.width = width;
         this.height = height;
     }
 
+    /**
+    * Gets color.
+    * @return color
+    */
     public Color getColor() {
         return color;
     }
 
+    /**
+    * Sets color.
+    * @param Color color color of tile
+    */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+    * Gets width.
+    * @return tile width
+    */
     public int getWidth() {
         return width;
     }
 
+    /**
+    * Sets width.
+    * @param int width width of tile
+    */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+    * Gets height.
+    * @return tile height
+    */
     public int getHeight() {
         return height;
     }
 
+    /**
+    * Sets height.
+    * @param int height tile height
+    */
     public void setHeight(int height) {
         this.height = height;
     }
